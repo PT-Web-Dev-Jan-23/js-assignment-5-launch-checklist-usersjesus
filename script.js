2925af9bc6ce219d8b9caacedc995fda4f319950
sjesus <s.jesus10@yahoo.com>
2023-03-07 20:27:46 -0600

    First Commit

--- script.js
+++ script.js
@@ -12,5 +12,15 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
+
+   let form = document.querySelector("form");
+   form.addEventListener("click", function(event) {
+    let pilotNameInput = document.querySelector("input[name=pilotName]");
+    if (pilotNameInput.value === "") {
+        alert("All fields are required!");
+        event.preventDefault();
+    }
+   })
+   
    
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,7 +17,9 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+   if (testInput === "") {
+    return "Empty"
+   } else if (isNaN(testInput))
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
